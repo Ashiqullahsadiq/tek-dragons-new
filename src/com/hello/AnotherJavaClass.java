077624f86c37af80c5aea457c3795da847ddd234
Ashiqullah Sadiq <ashiqullahsadiq44@gmail.com>
2023-05-18 00:45:22 -0700

    added new class

--- src/com/hello/AnotherJavaClass.java
+++ src/com/hello/AnotherJavaClass.java
@@ -3,7 +3,7 @@ package com.hello;
 public class AnotherJavaClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("My New Class in Java");
 
 	}
 
